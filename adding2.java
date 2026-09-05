9d82841ce3064403582787f8189cfb7377465267
harmeetbuilds <harmeetsingh230806@gmail.com>
2026-09-05 11:30:50 +0530

    more programs of java

---
+++ adding2.java
@@ -0,0 +1,9 @@
+public class adding2 {
+    public static void main(String a[])
+    {
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
---
+++ adding3.java
@@ -0,0 +1,16 @@
+public class adding3 {
+    public static void main(String[] args) {
+
+        int num1 = 9;
+        byte by = 127;
+        short sh = 558;
+        long l = 55584l;
+
+        float marks = 8.2f;
+        double d = 4.258;
+
+        char c = 'H';
+
+        boolean b = true;
+    }
+}
